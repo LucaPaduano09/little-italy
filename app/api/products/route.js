5575e541ed12afeed3f5fb85c71f4d730c9538e7
Luca Paduano <lucapaduano09@outlook.it>
2023-11-05 19:33:53 +0100

    wip 2

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,19 +2,19 @@ import { NextResponse } from "next/server";
 import connect from "../../../db";
 import Product from "../../../models/Product";
 
-export const GET = async (request) => {
-  try {
-    await connect();
-    const products = Product.find();
-    console.log(products);
-    throw new NextResponse(
-      "correctly responded from mongo, here's your products",
-      { status: 200 }
-    );
-  } catch (error) {
-    throw new NextResponse("error in fetching products - error: " + error, {
-      status: 500,
-    });
-    console.log(error);
-  }
-};
+// export const GET = async (request) => {
+//   // try {
+//   //   await connect();
+//   //   const products = Product.find();
+//   //   console.log(products);
+//   //   throw new NextResponse(
+//   //     "correctly responded from mongo, here's your products",
+//   //     { status: 200 }
+//   //   );
+//   // } catch (error) {
+//   //   throw new NextResponse("error in fetching products - error: " + error, {
+//   //     status: 500,
+//   //   });
+//   //   console.log(error);
+//   // }
+// };

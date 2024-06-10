049ca93087329b9d0cefbd62ab569330a84837da
Luca Paduano <l.paduano@reply.it>
2024-06-10 17:41:18 +0200

    Newsletter: wip 6

--- components/Newsletter/Newsletter.tsx
+++ components/Newsletter/Newsletter.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button, Image, Input } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const Newsletter = (props: any) => {
   const [timer, setTimer] = useState(false);
@@ -26,8 +27,13 @@ const Newsletter = (props: any) => {
       {(!props.session || props?.session?.user?.newsletter !== true) &&
         timer && (
           <>
-            <div className="fixed t-0 left-0 w-full h-full bg-black bg-opacity-40" />
-            <div className="h-[600px] fixed z-50 w-full bottom-20 left-0 bg-white flex items-center justify-center flex-col">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 2 }}
+              className="fixed t-0 left-0 w-full h-full bg-black bg-opacity-40"
+            />
+            <div className="h-[700px] fixed z-50 w-full bottom-20 left-0 bg-white flex items-center justify-center flex-col">
               <div className="flex flex-1 w-full">
                 <Image
                   src="./food-newsletter.jpg"
@@ -36,18 +42,25 @@ const Newsletter = (props: any) => {
                   radius="none"
                 />
               </div>
-              <div className="flex flex-1 w-full">
-                <Input onChange={(e) => setEmail(e.target.value)} />
-                <Button
-                  variant="shadow"
-                  className="w-full"
-                  color="primary"
-                  onClick={() => {
-                    handleNewsletterSubmit();
-                  }}
-                >
-                  Iscriviti
-                </Button>
+              <div className="flex flex-1 w-[90%] flex-col justify-center">
+                <h1 className="text-lg">Iscriviti alla newsletter</h1>
+                <h3>e ricevi subito il 15% di sconto sul tuo primo ordine</h3>
+                <div className="w-full flex-col flex items-center justify-center">
+                  <Input
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-[100%] my-4"
+                  />
+                  <Button
+                    variant="shadow"
+                    className="w-[100%]"
+                    color="primary"
+                    onClick={() => {
+                      handleNewsletterSubmit();
+                    }}
+                  >
+                    Iscriviti
+                  </Button>
+                </div>
               </div>
               <button
                 className="absolute top-2 right-2 w-6 h-6 rounded bg-red-300 z-50 flex items-center justify-center"

590d1c920db37d0268a612665096c6c0192f6235
Luca Paduano <l.paduano@reply.it>
2024-09-24 02:03:04 +0200

    fix17

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -2,24 +2,34 @@ import { NextResponse } from "next/server";
 import connect from "../../../../db";
 import Product from "../../../../models/Product";
 
-export const GET = async (req, res) => {
-  const url = req.url;
-  const id = url.replace(process.env.BASE_ENDPOINT + "/product/", "");
-  if (!id) {
-    return new NextResponse("Id non passato", { status: 400 });
-  }
-
+export const GET = async (req) => {
   try {
+    // Estrai l'URL e l'ID del prodotto
+    const url = new URL(req.url);
+    const id = url.pathname.split("/").pop();
+
+    // Verifica se l'ID è valido
+    if (!id) {
+      return new NextResponse("Id non passato", { status: 400 });
+    }
+
+    // Connessione al database
     await connect();
+
+    // Cerca il prodotto nel database
     const product = await Product.findById(id);
-    console.log(id);
-    if (product) {
-      return new NextResponse(JSON.stringify(product), { status: 200 });
-    } else {
+
+    if (!product) {
       return new NextResponse("Product not found", { status: 404 });
     }
+
+    // Ritorna il prodotto trovato
+    return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching product:", error);
-    res.status(500).json({ message: "Internal server error" + error });
+    return new NextResponse(
+      JSON.stringify({ message: "Internal server error: " + error.message }),
+      { status: 500 }
+    );
   }
 };
